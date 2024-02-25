6ef437dbce9a5fcfcf2e34c2e13d907a2cd6c98f
christianlsb <chrislsb09@gmail.com>
2024-02-25 15:07:26 -0300

    Adiciona roteamento para as telas de App e Cards

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./assets/styles.css";
+import { Cards } from "./screens";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/cartas",
+    element: <Cards />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
